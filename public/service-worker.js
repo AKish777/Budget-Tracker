0b0c012d66589adf69468b4193308a8a1547764f
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-09-23 20:01:13 -0400

    attempting to deploy

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,16 +5,19 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/styles.css",
     "/index.js",
+    "/db.js",
+    "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
 ];
 
 self.addEventListener("install", function(event) {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
     );
 
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
 
     self.skipWaiting();
@@ -26,13 +29,40 @@ self.addEventListener("activate", function(event) {
             return Promise.all(
                 keyList.map(key => {
                     if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old Cache!", key);
+                        console.log("Removing old cache data", key);
                         return caches.delete(key);
                     };
                 })
             );
         })
     );
-
     self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event) {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        if(response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(error => {
+                        return cache.match(event.request);
+                    });
+            }).catch(error => console.log(error))
+        );
+        return;
+    }
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        })
+    );
 });
